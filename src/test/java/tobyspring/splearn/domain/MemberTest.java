8d47e7871ee6d1ab913bb2117956949bf3b68b53
griotold <gotjd9773@naver.com>
2025-07-11 17:16:00 +0900

    Member 도메인 확장

--- src/test/java/tobyspring/splearn/domain/MemberTest.java
+++ src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -1,29 +1,40 @@
 package tobyspring.splearn.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Bean;
 
 import static org.assertj.core.api.Assertions.*;
 
 class MemberTest {
 
+    Member member;
+    PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        this.passwordEncoder = new PasswordEncoder() {
+            @Override
+            public String encode(String password) {
+                return password.toUpperCase();
+            }
+
+            @Override
+            public boolean matches(String password, String passwordHash) {
+                return encode(password).equals(passwordHash);
+            }
+        };
+        member = Member.create("rio@splearn.app", "rio", "secret", passwordEncoder);
+    }
+
     // 도메인 규칙을 잘 지키고 있는지 검증해 보는 것이다.
     @Test
     void createMember() {
-        var member = new Member("rio@splrean.app", "rio", "secret");
-
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
     }
 
     @Test
-    void constructorNullCheck() {
-        assertThatThrownBy(() -> new Member(null, "rio", "secret"))
-                .isInstanceOf(NullPointerException.class);
-    }
-
-    @Test
     void activate() {
-        Member member = new Member("rio@splrean.app", "rio", "secret");
-
         member.activate();
 
         assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
@@ -31,8 +42,6 @@ class MemberTest {
 
     @Test
     void activateFail() {
-        Member member = new Member("rio@splearn.app", "rio", "secret");
-
         member.activate();
 
         assertThatThrownBy(() -> member.activate())
@@ -41,8 +50,6 @@ class MemberTest {
 
     @Test
     void deactivate() {
-        Member member = new Member("rio@splearn.app", "rio", "secret");
-
         member.activate();
 
         member.deactivate();
@@ -52,8 +59,6 @@ class MemberTest {
     // 검증 하는 게 두 부분이지만, 간단하니까 같이 검증해본다.
     @Test
     void deactivateFail() {
-        Member member = new Member("rio@splearn.app", "rio", "secret");
-
         assertThatThrownBy(() -> member.deactivate()).isInstanceOf(IllegalStateException.class);
 
         member.activate();
@@ -62,4 +67,25 @@ class MemberTest {
         assertThatThrownBy(()-> member.deactivate()).isInstanceOf(IllegalStateException.class);
     }
 
+    @Test
+    void verifyPassword() {
+        assertThat(member.verifyPassword("secret", passwordEncoder)).isTrue();
+        assertThat(member.verifyPassword("false", passwordEncoder)).isFalse();
+    }
+
+    @Test
+    void changeNickname() {
+        assertThat(member.getNickname()).isEqualTo("rio");
+
+        member.changeNickname("mimi");
+
+        assertThat(member.getNickname()).isEqualTo("mimi");
+    }
+
+    @Test
+    void changePassword() {
+        member.changePassword("verySecret", passwordEncoder);
+        
+        assertThat(member.verifyPassword("verySecret", passwordEncoder)).isTrue();
+    }
 }
