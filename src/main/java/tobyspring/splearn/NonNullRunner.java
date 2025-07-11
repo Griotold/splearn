6ea21ccd1fb49750ac20229f67ee46be47e5f3d9
griotold <gotjd9773@naver.com>
2025-07-11 16:27:38 +0900

    feat: Add JSR-305 null safety annotations

---
+++ src/main/java/tobyspring/splearn/NonNullRunner.java
@@ -0,0 +1,15 @@
+package tobyspring.splearn;
+
+import org.springframework.lang.NonNull;
+
+public class NonNullRunner {
+
+    public static void main(String[] args) {
+        String name = null;
+        print(name);
+    }
+
+    static void print(@NonNull String input) {
+        System.out.println(input);
+    }
+}
